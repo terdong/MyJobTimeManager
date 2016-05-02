a8c140d9753bfe02926d410efea7310929e58d6c
DongHee Kim <donghee@piensoft.co.kr>
2016-05-02 15:49:07 +0900

    1. 최초 커밋

---
+++ MyJobTimeManager/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿using System.Windows.Forms;
+namespace MyJobTimeManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Gulim", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time Count:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Gulim", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(173, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "30:00";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Gulim", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(52, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Take a good rest!!!";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(143, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Quit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MyJobTimeManager.Properties.Resources.원피스;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MyJobTimeManager";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private Label label3;
+        private Button button3;
+
+    }
+}
+
---
+++ MyJobTimeManager/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyJobTimeManager
+{
+    public partial class Form1 : Form
+    {
+        private static readonly int Work_Time_Count_ = 30 * 60;
+        private static readonly int Rest_Time_Count_ = 5 * 60;
+        //private static readonly int Work_Time_Count_ = 5;
+        //private static readonly int Rest_Time_Count_ = 3;
+        private static readonly int Interval = 1000;
+        private static readonly int Second_Interval = Interval / 1000;
+        private TimeSpan TS_Work_Time_Count_;
+        private TimeSpan TS_Rest_Time_Count_;
+
+        private int second_current_type_count_;
+
+        private bool is_rest_time = false;
+        private FormWindowState current_form_state_;
+
+        private Timer timer_;
+        public Form1()
+        {
+            InitializeComponent();
+            Initialize();
+
+            this.notifyIcon1.Icon = Properties.Resources.Icon1;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Initialize()
+        {
+            TS_Work_Time_Count_ = TimeSpan.FromSeconds(Work_Time_Count_);
+            TS_Rest_Time_Count_ = TimeSpan.FromSeconds(Rest_Time_Count_);
+
+            SetTimeCountLabel(TS_Work_Time_Count_);
+            label3.Hide();
+
+            timer_ = new Timer() { Interval = 1000 };
+
+            timer_.Tick += (obj, args) =>
+            {
+                second_current_type_count_ = second_current_type_count_ - Second_Interval;
+                TimeSpan remaining = TimeSpan.FromSeconds(second_current_type_count_);
+
+                if (remaining.Seconds > Work_Time_Count_)
+                {
+                    //error
+                    new Exception("error");
+                }
+
+                SetTimeCountLabel(remaining);
+                
+                if (remaining.CompareTo(TimeSpan.Zero) < 1)
+                {
+                    if (is_rest_time)
+                    {
+                        ResetWorkTime();
+                    }
+                    else
+                    {
+                        second_current_type_count_ = Rest_Time_Count_;
+
+                        SetTimeCountLabel(TS_Rest_Time_Count_);
+
+                        label3.Show();
+
+                        if (WindowState.Equals(FormWindowState.Minimized))
+                        {
+                            ShowThisForm();
+                        }
+
+                        this.WindowState = FormWindowState.Maximized;
+                        this.FormBorderStyle = FormBorderStyle.None;
+                        this.TopMost = true;
+                    }
+
+                    is_rest_time = !is_rest_time;
+
+                }
+            };
+        }
+
+        private void ResetWorkTime()
+        {
+            second_current_type_count_ = Work_Time_Count_;
+
+            label3.Hide();
+
+            label2.Text = TS_Work_Time_Count_.ToString("mm\\:ss");
+
+            if (!timer_.Enabled) { timer_.Enabled = true; }
+            button1.Text = "Stop";
+
+            if (current_form_state_.Equals(FormWindowState.Minimized))
+            {
+                HideThisForm();
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.TopMost = false;
+            }
+        }
+
+        private void ResetRestTime()
+        {
+
+        }
+
+        private void SetTimeCountLabel(TimeSpan time_span)
+        {
+            label2.Text = time_span.ToString("mm\\:ss");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer_ != null)
+            {
+                if (timer_.Enabled)
+                {
+                    timer_.Stop();
+                    button1.Text = "Start";
+
+                }else
+                {
+                    ResetWorkTime();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+             if (WindowState.Equals(FormWindowState.Minimized))
+             {
+                 HideThisForm();
+                 current_form_state_ = WindowState;
+             }
+             else if(WindowState.Equals(FormWindowState.Normal))
+             {
+                 this.FormBorderStyle = FormBorderStyle.Sizable;
+                 current_form_state_ = WindowState;
+             }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowThisForm();
+        }
+
+        private void ShowThisForm()
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void HideThisForm()
+        {
+            this.Hide();
+            notifyIcon1.Visible = true;
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
